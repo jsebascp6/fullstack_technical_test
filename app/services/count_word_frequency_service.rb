afc98aa1548d4031338e6c9df9a3336e74bfbae3
Sebastian Castro <jsebascp6@gmail.com>
2022-01-31 22:16:47 -0500

    Printing number of cycles needed to find the answer

--- app/services/count_word_frequency_service.rb
+++ app/services/count_word_frequency_service.rb
@@ -16,10 +16,18 @@ class CountWordFrequencyService
   private
 
   def word_count
+    @n_g = 0
+    @n_t = 0
     text
       .gsub(/[^0-9A-Za-z]/, ' ')
       .split
-      .group_by { |word| word.downcase }
-      .transform_values(&:count)
+      .group_by { |word| @n_g=@n_g+1; print_cycles("group_by"); word.downcase }
+      .transform_values { |value| @n_t=@n_t+1; print_cycles("transform_values"); value.count }
+  end
+
+  def print_cycles(function)
+    puts function * 5
+    puts (function.eql?("group_by") ? @n_g : @n_t)
+    puts function * 5
   end
 end
--- app/services/hash_service.rb
+++ app/services/hash_service.rb
@@ -1,10 +1,20 @@
 class HashService
 
   def self.convert_hash_to_array_ordered(hash)
-    new_array = []
-    hash.each { |key, value| new_array << [key, value]}
+    @n_e = 0
+    @n_s = 0
 
-    new_array.sort_by { |_, value| -value.to_i }
+    new_array = []
+    hash.each { |key, value| @n_e=@n_e+1; print_cycles("each"); new_array << [key, value]}
+
+    new_array.sort_by { |_, value| @n_s=@n_s+1; print_cycles("short_by"); -value.to_i }
   end
 
+  private
+
+  def self.print_cycles(function)
+    puts function * 5
+    puts (function.eql?("each") ? @n_e : @n_s)
+    puts function * 5
+  end
 end
